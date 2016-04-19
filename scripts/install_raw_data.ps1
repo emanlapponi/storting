8d41e2d62c9401fff643bf1ce5092edce1f7136d
martigso <martin_soyland@hotmail.com>
2016-04-19 22:44:27 +0200

    Including PowerShell script for raw data installation

---
+++ scripts/install_raw_data.ps1
@@ -0,0 +1,13 @@
+﻿#### Relies on 7Zip4Powershell ####
+# Install-Package 7Zip4Powershell
+
+echo "Downloading data..."
+wget http://emanuel.at.ifi.uio.no:/storting_raw.tar -OutFile storting_raw.tar
+
+echo "Unpacking..."
+Expand-7Zip -ArchiveFileName storting_raw.tar -TargetPath .\data
+
+echo "Deleting tarball"
+Remove-Item .\storting_raw.tar
+
+echo "Done"
