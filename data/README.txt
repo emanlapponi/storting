f68c4dac415d2dc33bcf36f52734dd6052739f51
Emanuele Lapponi <emanlapponi@gmail.com>
2016-04-25 14:36:57 +0200

    brutecommit
    brutecommit

--- data/README.txt
+++
@@ -1,2 +0,0 @@
-To get the raw html data, from the top level directory, call:
-./scripts/install_raw_data.sh
--- scripts/install_raw_data.sh
+++
@@ -1,8 +0,0 @@
-echo 'downloading...'
-wget http://emanuel.at.ifi.uio.no:/storting_raw.tar;
-echo 'unpacking...'
-tar xf storting_raw.tar;
-mv stortinget.no/ data/
-echo 'deleting tarball...'
-rm storting_raw.tar
-echo 'done!'
