b5e6dc73d3a01ac26422b325d2f9d9710fd62f5f
martigso <martin_soyland@hotmail.com>
2016-04-25 21:08:39 +0200

    Fixed unique speaker list and some R-code

--- R/Scripts/party_data_prep.R
+++ R/Scripts/party_data_prep.R
@@ -1,6 +1,5 @@
 rm(list = ls());cat("\014")
-library(uacd);library(dplyr);library(rvest)
-
+library(stringr);library(uacd);library(dplyr);library(rvest);library(parallel)
 
 # Do we need this?
 # cab_grid <- expand.grid(data.frame(party_name = levels(factor(seats$party_name)), session = levels(factor(seats$session))))
@@ -11,7 +10,7 @@ library(uacd);library(dplyr);library(rvest)
 load("Data/norCabinet.rda")
 load("./Data/legislators.rda")
 
-legislators <- legislators[which(as.numeric(legislators$period) >= 1997), 
+legislators <- legislators[which(as.numeric(legislators$period) >= 1990), 
                            c("name", "period", "initials", "state", "party", "gender", "birth", "death")]
 
 names(legislators)
@@ -23,7 +22,7 @@ taler <- taler[, 1:7]
 taler$date <- as.Date(taler$time)
 
 legislators$party <- factor(legislators$party, labels = c("A", "FrP", "H", "Kp", "KrF", "MDG", "Sp", "SV", "TF", "V"))
-summary(legislators$party)
+summary(factor(legislators$party))
 
 
 # Danger!!! ###########################
@@ -44,26 +43,137 @@ parties <- strsplit(norCabinet$CabinetPartiesNor, "\\+")
 norCabinet$composition <- ifelse(sapply(parties, function(x) length(x)) <= 1, "Single-party", "Coalition")
 
 rm(parties)
-str(norCabinet)
-names(legislators)
 
 
 taler$name <- as.character(taler$name)
 name_merge <- strsplit(taler$name, " ")
 
-name_merge <- lapply(name_merge, function(x) rev(x))
+name_merge2 <- lapply(name_merge, function(x) rev(x))
+name_merge2 <- unlist(lapply(name_merge, function(x) ifelse(length(x) != 0, rev(x)[1], x)))
 
-# for(i in 1:length(taler$name_merge)){
-#   taler$name_merge[[i]][1] <- paste0(toupper(taler$name_merge[[i]][1]), ",")
-# }
+name_merge <- lapply(name_merge, function(x) str_trim(paste(x, collapse = " ")))
+name_merge <- sapply(1:length(name_merge), function(x) gsub(name_merge2[[x]], "", name_merge[[x]]))
+name_merge <- paste(toupper(name_merge2), name_merge, sep = ", ")
+taler$name_merge <- str_trim(name_merge)
 
-name_merge_one <- lapply(1:length(name_merge), function(x) paste0(toupper(name_merge[[x]][1]), ","))
-name_merge_one[1]
-
-taler$name_merge <- unlist(lapply(1:length(name_merge), function(x) paste0(name_merge_one[[x]], " ", paste(name_merge[[x]][-1], collapse = " "))))
 taler$name_merge <- ifelse(taler$name == "Presidenten", taler$name, taler$name_merge)
-cbind(taler$name[1:100], taler$name_merge[1:100])
+taler$name_merge <- ifelse(taler$name_merge == "BALLO, Gunnar", "BALLO, Olav Gunnar", taler$name_merge)
+taler$name_merge <- ifelse(taler$name_merge == "BALLO, Ole Gunnar", "BALLO, Olav Gunnar", taler$name_merge)
+
+
 
 speakers <- paste(levels(factor(taler$name_merge)), collapse = "\n")
+
 cat(speakers)
-writeLines(speakers, "./Data/speakers.txt")
+writeLines(speakers, "./Data/speakers")
+
+isitthere <- mclapply(taler$name_merge, function(x) any(grepl(x, legislators$name)), mc.cores = 6)
+
+isitthere <- unlist(isitthere)
+levels(factor(taler$name_merge))[1:10]
+notfound <- which(isitthere == FALSE)
+
+tester <- taler[notfound, c("name", "name_merge", "title", "date")]
+
+tester <- tester[-which(tester$name == "Presidenten" | tester$title == "Statsråd" | duplicated(tester$name)), ]
+
+tester <- tester[-which(tester$date > as.Date("2013-10-01")), ]
+
+tester <- arrange(tester, name_merge)
+
+
+legislators[which(grepl("ANDERSEN", legislators$name)), ]
+
+
+
+tester$id <- NA
+tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+tester$id[which(grepl("ÅMLAND", tester$name_merge))] <- "TAML"
+tester$id[which(grepl("ANDERSEN, Geir", tester$name_merge))] <- "GEA"
+tester$id[which(grepl("ANDERSEN, Vidar", tester$name_merge))] <- "VA"
+tester$id[which(grepl("ANDREASSEN, Brit I. H.", tester$name_merge))] <- "BIA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+# tester$id[which(grepl("AKSNES", tester$name_merge))] <- "MGRA"
+
+
+
+
+
+
