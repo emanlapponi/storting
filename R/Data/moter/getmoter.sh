f4e51ae843041557d0bfbd706752e26a52790a9d
martigso <martin_soyland@hotmail.com>
2016-09-18 18:23:26 -0400

    Updated meeting crawler

--- R/Data/moter/getmoter.sh
+++ R/Data/moter/getmoter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sessions=(2013-2017 2009-2013 2005-2009 2001-2005 1997-2001)
 session_start=(`seq 1998 1 2016`)
 session_end=(`seq 1999 1 2017`)
 index=(`seq 1 1 19`)
@@ -12,3 +11,9 @@ echo ${session[@]}
 for i in ${session[@]}; do
   wget "https://data.stortinget.no/eksport/moter?sesjonid="$i
 done
+
+# beacuse of non-standard link the 1998/1999 session is downloaded manually
+wget "https://data.stortinget.no/eksport/moter?sesjonid=1998-99"
+
+# and reanaming
+mv moter\?sesjonid=1998-99 moter\?sesjonid=1998-1999
