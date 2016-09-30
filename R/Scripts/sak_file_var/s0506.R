683a31b5bf11d71a1ff5512b78a53a96dabf746f
martigso <martin_soyland@hotmail.com>
2016-09-30 15:40:13 -0400

    Added session scripts for matching debates to raw html of stortinget.no

--- R/Scripts/sak_file_var/s0506.R
+++ R/Scripts/sak_file_var/s0506.R
@@ -1,7 +1,7 @@
 source("./Scripts/sak_file_var/getMeta.R")
 
 soFar <- getMeta(pathToData = "../../taler/id_taler_meta.csv",
-                 sakfolderPath = "/media/martin/Data/saker_raw/stortinget.no/no/Saker-og-publikasjoner/Publikasjoner/Referater/Stortinget/",
+                 sakfolderPath = "/media/martin/Data/referat_raw/stortinget.no/no/Saker-og-publikasjoner/Publikasjoner/Referater/Stortinget/",
                  session = "2005-2006")
 
 save(soFar, file = paste0("./Data/sak_filerefs/sakfiles", unique(soFar$session), ".rda"))
